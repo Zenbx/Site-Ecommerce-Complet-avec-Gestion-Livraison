964536261dd5636e0de031d600a4e1f1eca75eba
BELEKOTAN II JEFF NICHOSS <jeffbelekotan@gmail.com>
2026-01-08 10:11:06 +0100

    Fix: Remove TelescopeServiceProvider completely for production

--- ecommerce-site-laravel/bootstrap/providers.php
+++ ecommerce-site-laravel/bootstrap/providers.php
@@ -2,9 +2,4 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-
-    // Load Telescope only in local environment
-    ...app()->environment('local') ? [
-        App\Providers\TelescopeServiceProvider::class,
-    ] : [],
 ];
